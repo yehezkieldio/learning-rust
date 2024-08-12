935d5cb7042cce69c08d48d0bcbaa43d0a77a27d
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-12 09:40:37 +0800

    feat: more vec

--- vector/src/main.rs
+++ vector/src/main.rs
@@ -52,4 +52,26 @@ fn main() {
     // init vector with a certain size
     let zeros = vec![0; 5];
     println!("{:?}", zeros);
+
+    // remove some elements and shift all that come after
+    let mut nums = vec![1, 2, 3, 4, 5];
+    let second_num = nums.remove(1);
+    println!("{:?}", nums);
+    println!("The second number is {:?}", second_num);
+
+    // filter elements
+    let names = vec!["John", "Jane", "Joe", "Jill", "Jack", "Alex"];
+
+    // only keep names that start with J
+    // names is moved here, so we can't use it anymore
+    // into_iter() consumes the vector and returns an iterator
+    // filter() creates a new iterator that only contains elements that satisfy the condition
+    // chars() returns an iterator over the characters of the string, nth(0) returns the first character
+    // collect() consumes the iterator and creates a new vector
+    let j_names = names
+        .into_iter()
+        .filter(|name| name.chars().nth(0).unwrap() == 'J')
+        .collect::<Vec<_>>();
+
+    println!("{:?}", j_names);
 }
