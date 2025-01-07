3ecd5c034308b55f2a9ebb0835e5672ddc34ebe5
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-01-07 19:19:57 +0800

    feat: ndarray multidimensional arrays

--- ndarray-data-analysis-manipulation/src/main.rs
+++ ndarray-data-analysis-manipulation/src/main.rs
@@ -1,4 +1,4 @@
-use ndarray::{Array, ShapeBuilder};
+use ndarray::{Array, Array3, ShapeBuilder, array};
 
 fn initial_placeholders() {
     // create a 1x4 array of zeros
@@ -30,6 +30,37 @@ fn initial_placeholders() {
     println!("{:?}", eye);
 }
 
+fn multidimensional_arrays() {
+    let array_1d = Array::from_vec(vec![1., 2., 3., 4.]);
+    println!("{:?}", array_1d);
+
+    let array_d11 = Array::from_shape_vec((1, 4), vec![1, 2, 3, 4]);
+    println!("{:?}", array_d11.unwrap());
+
+    let array_2d = array![
+        [-1.01, 0.86, -4.60, 3.31, -4.81],
+        [3.98, 0.53, -7.04, 5.29, 3.55],
+        [3.30, 8.26, -3.89, 8.20, -1.51],
+        [4.43, 4.96, -7.66, -7.33, 6.18],
+        [7.31, -6.43, -6.16, 2.47, 5.58],
+    ];
+    println!("{:?}", array_2d);
+
+    let array_d2 = Array::from_shape_vec((2, 2), vec![1., 2., 3., 4.]);
+    // 1. or 2. with a trailing dot is a float in Rust
+    println!("{:?}", array_d2.unwrap());
+
+    let data = vec![1., 2., 3., 4., 5., 6., 7., 8., 9.];
+    let array_2d1 = Array::from_shape_vec((3, 3), data);
+    println!("{:?}", array_2d1.unwrap());
+
+    let data = vec![1., 2., 3., 4.];
+    let array_3d = Array3::from_shape_vec((2, 2, 1), data);
+    println!("{:?}", array_3d.unwrap());
+}
+
 fn main() {
     initial_placeholders();
+    println!();
+    multidimensional_arrays();
 }
